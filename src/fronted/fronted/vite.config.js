42ccb9afbc12f8487fa4c80914f43164030e7c05
geoffrey0pv <geoffreypv00@gmail.com>
2024-11-18 15:34:36 -0500

    fix main

--- src/fronted/fronted/vite.config.js
+++ src/fronted/fronted/vite.config.js
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  base: './', // Esto asegura que las rutas sean relativas en producción
   css: {
     postcss: {
       plugins: [tailwindcss()],
