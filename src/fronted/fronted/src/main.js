a125591339a9434b4ffc287df81fdce066a1d027
geoffrey0pv <geoffreypv00@gmail.com>
2024-11-18 11:37:38 -0500

    fix: main.js

--- src/fronted/fronted/src/main.js
+++ src/fronted/fronted/src/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';  // Change this import to use createRoot
 import { Provider } from 'react-redux';
 import store from './redux/store.jsx'; // Asegúrate de que el store esté correctamente configurado
-import App from './App';
+import App from './App.jsx';
 
 // Create a root and render the app
 const root = ReactDOM.createRoot(document.getElementById('root'));
