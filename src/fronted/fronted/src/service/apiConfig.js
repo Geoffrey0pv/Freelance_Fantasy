dcee1b8065b6a3377b294cbed6df43bb1e162db4
geoffrey0pv <geoffreypv00@gmail.com>
2024-11-19 10:13:26 -0500

    fix: url base api

--- src/fronted/fronted/src/service/apiConfig.js
+++ src/fronted/fronted/src/service/apiConfig.js
@@ -2,4 +2,6 @@
 const isDevelopment = import.meta.env.MODE === 'development';
 
 // Define la base URL en función del entorno
-export const BASE_API_URL = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_PROD; // URL de producción
+export const BASE_API_URL = "freelancefantasy-backend-dfbte3d4f0csd5bc.westus-01.azurewebsites.net";
+
+// isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_PROD; // URL de producción
