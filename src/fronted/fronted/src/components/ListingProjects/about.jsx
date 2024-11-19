be1467110c7d0f6f66f772494c725a9fa9869b1f
geoffrey0pv <geoffreypv00@gmail.com>
2024-11-19 11:20:31 -0500

    fix: urls

--- src/fronted/fronted/src/components/ListingProjects/about.jsx
+++ src/fronted/fronted/src/components/ListingProjects/about.jsx
@@ -35,7 +35,7 @@ const About = () => {
 
                 <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
-                        <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
+                        <img src="/assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
 
                         <div>
                             <p className="grid-headtext">Nuestra Tecnología</p>
@@ -79,7 +79,7 @@ const About = () => {
 
                 <div className="xl:col-span-2 xl:row-span-3">
                     <div className="grid-container">
-                        <img src="grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+                        <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
 
                         <div>
                             <p className="grid-headtext">¿Por qué Freelance Fantasy?</p>
@@ -95,7 +95,7 @@ const About = () => {
                 <div className="xl:col-span-1 xl:row-span-2">
                     <div className="grid-container">
                         <img
-                            src="/grid4.png"
+                            src="/assets/grid4.png"
                             alt="grid-4"
                             className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
                         />
--- src/fronted/fronted/src/service/apiConfig.js
+++ src/fronted/fronted/src/service/apiConfig.js
@@ -2,6 +2,6 @@
 const isDevelopment = import.meta.env.MODE === 'development';
 
 // Define la base URL en función del entorno
-export const BASE_API_URL = "freelancefantasy-backend-dfbte3d4f0csd5bc.westus-01.azurewebsites.net";
+export const BASE_API_URL = "https://freelancefantasy-backend-dfbte3d4f0csd5bc.westus-01.azurewebsites.net";
 
 // isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_PROD; // URL de producción
