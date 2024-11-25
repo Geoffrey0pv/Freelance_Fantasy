33b32566121b16d0c707cdee99886447fe738b3d
geoffrey0pv <geoffreypv00@gmail.com>
2024-11-25 15:49:09 -0500

    fix fixes

--- src/fronted/fronted/src/components/Profile/View/ProfileImageSection.jsx
+++ src/fronted/fronted/src/components/Profile/View/ProfileImageSection.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 const ViewProfileImageSection = ({ profileImage, coverImage }) => {
   const baseUrl = "https://freelancefantasy-backend-dfbte3d4f0csd5bc.westus-01.azurewebsites.net"; // Define la base URL
-
+  const cleanedBannerUrl1 = profileImage.replace("users/profile/", "");
+  const cleanedBannerUrl2 = coverImage.replace("users/profile/", "");
   return (
     <div className="relative w-full h-48 bg-gray-300 rounded-lg shadow-lg">
       {coverImage ? (
         <img
-          src={`${baseUrl}${coverImage}`}
+          src={cleanedBannerUrl1}
           alt="Portada"
           className="w-full h-full object-cover rounded-lg"
         />
@@ -18,7 +19,7 @@ const ViewProfileImageSection = ({ profileImage, coverImage }) => {
       <div className="absolute left-1/2 transform -translate-x-1/2 -bottom-12 w-24 h-24 rounded-full border-4 border-gray-300 overflow-hidden">
         {profileImage ? (
           <img
-            src={`${baseUrl}${profileImage}`}
+            src={cleanedBannerUrl2}
             alt="Perfil"
             className="w-full h-full object-cover"
           />
